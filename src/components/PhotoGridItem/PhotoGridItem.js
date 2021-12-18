04b9e02cd587850e5c525d7e5383f36de3a2ca1a
Julia <juliadmartin720@gmail.com>
2021-12-17 19:08:55 -0800

    Restrict tags to one line

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -40,17 +40,23 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  // display: flex;
+  // flex-wrap: wrap;
+  // gap: 8px;
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 const Tag = styled.li`
+  display: inline-block;
   padding: 4px 8px;
+  margin-right: 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  text-overflow: ellipsis;
+  overflow: hidden;
 `;
 
 export default PhotoGridItem;
